1e70e72259487cd04766ba50557aafbc1b372db7
HemavathyLakisha <hemaluki@gmail.com>
2026-01-30 14:53:47 +0530

    Task created for optional class

--- src/Java8/OptionalClassDemo.java
+++ src/Java8/OptionalClassDemo.java
@@ -1,44 +1,57 @@
 package Java8;
-class Person{
-	String name;
-	Bike b;
-	public Person(String name,Bike b) {
-		super();
-		this.name=name;
-		this.b=b;
-	}
-	public String getName() {
-		return name;
-	}
-	public Bike getB() {
-		return b;
-	}
-	public String getBikeName(Bike b) {
-		Person p1=p;
-	}
-	
-}
-class Bike{
-	String bikename;
 
-	public Bike(String bikename) {
-		super();
-		this.bikename = bikename;
-	}
-	
-	public String getBikename() {
-		return bikename;
-	}
+import java.util.Optional;
+
+class Person {
+    private String name;
+    private Bike bike;
+
+    public Person(String name, Bike bike) {
+        this.name = name;
+        this.bike = bike;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Optional<Bike> getBike() {
+        return Optional.ofNullable(bike);
+    }
 }
+
+class Bike {
+    private String bikename;
+
+    public Bike(String bikename) {
+        this.bikename = bikename;
+    }
+
+    public String getBikename() {
+        return bikename;
+    }
+}
+
 public class OptionalClassDemo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Person joyal=new Person("Joyal",new Bike("KTM"));
-		System.out.println(joyal.getBikeName(joyal));
+    // Utility method using Optional chaining
+    public static String getBikeName(Person person) {
 
-		Person k7=new Person("k7",null);
-		System.out.println(joyal.getBikeName(k7));
-	}
+        return Optional.ofNullable(person)          // Person may be null
+                .flatMap(Person::getBike)           // Bike may be null
+                .map(Bike::getBikename)              // get bike name
+                .orElse("No Bike Assigned");          // default value
+    }
 
+    public static void main(String[] args) {
+
+        Person joyal = new Person("Joyal", new Bike("KTM"));
+        System.out.println(getBikeName(joyal));
+
+        Person k7 = new Person("k7", null);
+        System.out.println(getBikeName(k7));
+
+        Person unknown = null;
+        System.out.println(getBikeName(unknown));
+    }
 }
